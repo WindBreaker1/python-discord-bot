f72b82dfe90055d92fe2d99ee529f4ca0b9a0f9f
WindBreaker1 <dokgojungwoo@gmail.com>
2025-01-23 09:00:30 +0200

    main branch test

--- guild_config.py
+++ guild_config.py
@@ -1,2 +1,4 @@
 # This file contains the guild ID for testing.
-TEST_GUILD_ID = 1206133484423483453  # Replace with your test server ID
+#TEST_GUILD_ID = 1206133484423483453  # Replace with your test server ID
+
+TEST_GUILD_ID = None
